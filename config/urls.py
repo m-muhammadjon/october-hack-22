9deb7c3fcba33494468e3f61d8e5dde6077f0f1a
Mukhammadjon <muhammadjon.200415@gmail.com>
2022-11-05 11:52:49 +0500

    changed api url

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from config.routing import urlpatterns as api_urls
 from config.swagger import urlpatterns as swagger_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('config.routing')),
 ]
 
 urlpatterns += swagger_urls
-urlpatterns += api_urls
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
